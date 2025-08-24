2bbc14da2a8df77805053e5904e40734f689e369
Miguel Angel Acuna <macunahernandez597@gmail.com>
2025-08-23 20:16:16 -0500

    container

--- src/views/servidores/ListaServidores.vue
+++ src/views/servidores/ListaServidores.vue
@@ -1,65 +1,66 @@
 <template>
-  <v-card class="pa-4 elevation-2 list-container bg-blur">
-    <h2 class="text-h6 font-weight-bold mb-4">Listado de Servidores</h2>
+  <v-container>
+    <v-card class="pa-4 elevation-2 list-container bg-blur">
+      <h2 class="text-h6 font-weight-bold mb-4">Listado de Servidores</h2>
 
-    <v-data-table
-      :headers="headers"
-      :items="servidores"
-      :loading="loading"
-      class="elevation-1 servidores-table"
-      density="comfortable"
-    >
-      <!-- Estado activo/inactivo -->
-      <template #item.activo="{ item }">
-        <transition name="scale-fade">
-          <v-chip
-            v-if="item"
-            small
-            variant="outlined"
-            :color="item.activo ? 'success' : 'error'"
-            label
-            text-color="white"
-            class="ma-1"
-          >
-            {{ item.activo ? 'Activo' : 'Inactivo' }}
-          </v-chip>
-        </transition>
-      </template>
+      <v-data-table
+        :headers="headers"
+        :items="servidores"
+        :loading="loading"
+        class="elevation-1 servidores-table"
+        density="comfortable"
+      >
+        <!-- Estado activo/inactivo -->
+        <template #item.activo="{ item }">
+          <transition name="scale-fade">
+            <v-chip
+              v-if="item"
+              small
+              variant="outlined"
+              :color="item.activo ? 'success' : 'error'"
+              label
+              text-color="white"
+              class="ma-1"
+            >
+              {{ item.activo ? 'Activo' : 'Inactivo' }}
+            </v-chip>
+          </transition>
+        </template>
 
-      <!-- Disponibilidad de aseo -->
-      <template #item.disponibilidad_aseo="{ item }">
-        <transition name="scale-fade">
-          <v-chip
-            v-if="item"
-            small
-            variant="outlined"
-            :color="item.disponibilidad_aseo ? 'success' : 'error'"
-            label
-            text-color="white"
-            class="ma-1"
-          >
-            {{ item.disponibilidad_aseo ? 'Sí' : 'No' }}
-          </v-chip>
-        </transition>
-      </template>
+        <!-- Disponibilidad de aseo -->
+        <template #item.disponibilidad_aseo="{ item }">
+          <transition name="scale-fade">
+            <v-chip
+              v-if="item"
+              small
+              variant="outlined"
+              :color="item.disponibilidad_aseo ? 'success' : 'error'"
+              label
+              text-color="white"
+              class="ma-1"
+            >
+              {{ item.disponibilidad_aseo ? 'Sí' : 'No' }}
+            </v-chip>
+          </transition>
+        </template>
 
-      <!-- Columna de acciones -->
-      <template #item.acciones="{ item }">
-        <v-btn icon size="small" color="primary" variant="text" @click="verDetalle(item)">
-          <v-icon>mdi-eye</v-icon>
-        </v-btn>
-        <v-btn icon size="small" color="secondary" variant="text" @click="editar(item)">
-          <v-icon>mdi-pencil</v-icon>
-        </v-btn>
-      </template>
+        <!-- Columna de acciones -->
+        <template #item.acciones="{ item }">
+          <v-btn icon size="small" color="primary" variant="text" @click="verDetalle(item)">
+            <v-icon>mdi-eye</v-icon>
+          </v-btn>
+          <v-btn icon size="small" color="secondary" variant="text" @click="editar(item)">
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </template>
 
-      <!-- Loader animado -->
-      <template #loading>
-        <v-skeleton-loader type="table-row" class="w-100" :loading="loading" :rows="5" />
-      </template>
-    </v-data-table>
-  </v-card>
-
+        <!-- Loader animado -->
+        <template #loading>
+          <v-skeleton-loader type="table-row" class="w-100" :loading="loading" :rows="5" />
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
   <Notificacion ref="notificacionRef" />
 </template>
 
